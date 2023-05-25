df85b21061724f05c8d2d16b4fab521079046a31
José Fábio <jfsant4@gmail.com>
2023-05-25 19:49:52 -0300

    Criação da pasta config e arquivo custom_colors

---
+++ lib/config/custom_colors.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+abstract class CustomColors {
+  static Color customColorWhite = Colors.white;
+  static Color customColorWhitealpha200 = Colors.white.withAlpha(200);
+  static Color CustomColorTeal = Colors.teal;
+  static Color CustomColorGrey = Colors.grey;
+}
